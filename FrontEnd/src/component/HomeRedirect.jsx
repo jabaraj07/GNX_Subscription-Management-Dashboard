e9d326d7a1113bf035b6d74e4b2af90cb86bcd8c
SamuelRaj-tech <samuelrajk76@gmail.com>
2026-04-13 22:23:52 +0530

    HomeRedirect code change

--- FrontEnd/src/component/HomeRedirect.jsx
+++ FrontEnd/src/component/HomeRedirect.jsx
@@ -2,7 +2,8 @@ import { Navigate } from "react-router-dom";
 import useAuthStore from "../store/authStore";
 
 const HomeRedirect = () => {
-  const { isAuthenticated, user } = useAuthStore();
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const user = useAuthStore((state) => state.user);
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
