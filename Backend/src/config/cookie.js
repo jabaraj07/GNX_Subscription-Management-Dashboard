df04f650112cb7e43fdf55346ed950100248fc64
SamuelRaj-tech <samuelrajk76@gmail.com>
2026-04-13 21:51:37 +0530

    change cookie config

--- Backend/src/config/cookie.js
+++ Backend/src/config/cookie.js
@@ -2,13 +2,13 @@ export const cookieConfig = {
   access: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: Number(process.env.ACCESS_COOKIE_MAX_AGE),
   },
   refresh: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: Number(process.env.REFRESH_COOKIE_MAX_AGE),
   }
 };
